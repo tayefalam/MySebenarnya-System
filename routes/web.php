79f7e48746c6a78f18c457c929618592a6f2a4ca
amirahhannan <amirahhannanhasan@gmail.com>
2025-06-23 01:25:04 +0800

    m2 routes

--- routes/web.php
+++ routes/web.php
@@ -157,7 +157,7 @@ Route::middleware(['web'])->group(function () {
         Route::middleware(['check.user.type:MCMC'])->group(function () {
         Route::get('/review-inquiries', [ReviewReportController::class, 'index'])->name('inquiries.review');// View and review submitted inquiries
         Route::post('/inquiries/{id}/status', [ReviewReportController::class, 'updateStatus'])->name('inquiries.updateStatus');// Update inquiry status (accepted / Rejected)
-        Route::get('/reports', [ReviewReportController::class, 'report'])->name('inquiries.report');// Generate report based on inquiries
+        Route::get('/report-inquiries', [ReviewReportController::class, 'report'])->name('inquiries.report');// Generate report based on inquiries
         Route::get('/report/download', [ReviewReportController::class, 'downloadPDF'])->name('inquiries.report.download');//Download report as PDF
         });
 
